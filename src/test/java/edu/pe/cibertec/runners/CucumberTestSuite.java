94fc375d3d6ab927625cf43d998ddc187a896666
Christiam Mendoza <ch.alb.rz@gmail.com>
2025-12-03 21:38:06 -0500

    fix package

--- src/test/java/edu/pe/cibertec/runners/CucumberTestSuite.java
+++ src/test/java/edu/pe/cibertec/runners/CucumberTestSuite.java
@@ -1,4 +1,4 @@
-package edu.pe.cibiertec.runners;
+package edu.pe.cibertec.runners;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
--- src/test/java/edu/pe/cibertec/steps/ProductsSteps.java
+++ src/test/java/edu/pe/cibertec/steps/ProductsSteps.java
@@ -1,4 +1,4 @@
-package edu.pe.cibiertec.steps;
+package edu.pe.cibertec.steps;
 
 
 
